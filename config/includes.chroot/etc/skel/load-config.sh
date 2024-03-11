de5a3402fd9781cfa4d1504f646d5928cc7c4c13
Mattia Peronio <mattia.peronio@gmail.com>
2024-03-11 23:32:08 +0100

    bugfix

--- config/includes.chroot/etc/skel/load-config.sh
+++ config/includes.chroot/etc/skel/load-config.sh
@@ -24,7 +24,7 @@ for ((i = 0; i <= 9; i++)); do
     	    session_options=${!session_options_varname}
 	    echo "Found $session_options_varname = $session_options"
 	    echo "Xterm params = $session_type $session_server $session_options"
-            xterm -e $session_type $session_server $session_options &
+            xterm -e $session_type $session_server "$session_options" &
 	fi
 
 
